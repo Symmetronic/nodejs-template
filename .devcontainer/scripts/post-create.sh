ff2c12d311b2234799ed4e59f29c979c0f680497
Simon Hochholzer <simon@symmetronic.com>
2026-06-04 20:10:10 +0000

    chore: Remove unnecessary Git configuration

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -5,9 +5,5 @@ set -e
 sudo mkdir -p /workspace/node_modules
 sudo chown -R node:node /workspace/node_modules
 
-# Configure Git
-git config --global --add safe.directory /workspace
-git config core.fileMode false
-
 # Install dependencies
 npm ci
